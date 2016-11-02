72dd72353014a6d36e6f40484faede60c5c54fcd
Brunno Moreira <brunnomoreira.silva@gmail.com>
2016-11-02 18:15:40 -0300

    testando heroku

---
+++ app/modules/dashboard/controllers/TestController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Spider\Modules\Dashboard\Controllers;
+
+class TestController extends ControllerBase
+{
+    public function indexAction()
+    {
+        
+    }
+    
+    public function testeAction()
+    {
+        echo "Outro Teste Heroku"; 
+    }
+}
