978f3e8e1a4e643049cd33f95cf73621e605a307
Brunno Moreira <brunnomoreira.silva@gmail.com>
2016-11-24 15:02:57 -0300

    migrations

---
+++ app/migrations/1.0.2/user.php
@@ -0,0 +1,112 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+class UserMigration_102 extends Migration
+{
+
+    public function up()
+    {
+        $this->morphTable(
+            'user',
+            array(
+            'columns' => array(
+                new Column(
+                    'id',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'notNull' => true,
+                        'autoIncrement' => true,
+                        'size' => 11,
+                        'first' => true
+                    )
+                ),
+                new Column(
+                    'idRole',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'notNull' => true,
+                        'size' => 11,
+                        'after' => 'id'
+                    )
+                ),
+                new Column(
+                    'name',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 100,
+                        'after' => 'idRole'
+                    )
+                ),
+                new Column(
+                    'username',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 100,
+                        'after' => 'name'
+                    )
+                ),
+                new Column(
+                    'password',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 100,
+                        'after' => 'username'
+                    )
+                ),
+                new Column(
+                    'email',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 100,
+                        'after' => 'password'
+                    )
+                ),
+                new Column(
+                    'phone',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 25,
+                        'after' => 'email'
+                    )
+                ),
+                new Column(
+                    'cellphone',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 25,
+                        'after' => 'phone'
+                    )
+                ),
+                new Column(
+                    'active',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 1,
+                        'after' => 'cellphone'
+                    )
+                )
+            ),
+            'indexes' => array(
+                new Index('PRIMARY', array('id'))
+            ),
+            'options' => array(
+                'TABLE_TYPE' => 'BASE TABLE',
+                'AUTO_INCREMENT' => '3',
+                'ENGINE' => 'InnoDB',
+                'TABLE_COLLATION' => 'utf8_general_ci'
+            )
+        )
+        );
+    }
+}
